231ef72af997799dee50388dd3c6bb1c59a2b44a
Elton Luiz <eltonluiz12042012@gmail.com>
2023-06-27 23:54:36 -0300

    Upload de imagens

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from galeria.models import Fotografia
 
 # Register your models here.
+
+@admin.register(Fotografia)
+class FotografiaAdmin(admin.ModelAdmin):
+    list_display = ('id','nome', 'legenda','categoria', 'foto','data_fotografia' ,'publicada', )
+    list_display_links = ('id','nome',)
+    fields =  ('nome', 'legenda','categoria',  'descricao', 'foto', 'publicada','data_fotografia')
+    list_per_page = 10
+    list_filter = ('categoria',)
+    search_fields = ('nome', 'legenda')
+    list_editable = ('publicada',)
+    #ordering = ('id','nome')
