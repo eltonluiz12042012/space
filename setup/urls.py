80f016d8332abe91268ad571d9f1d66c26e55cb8
Elton Luiz <eltonluiz12042012@gmail.com>
2023-06-28 23:37:03 -0300

    App de users

--- setup/urls.py
+++ setup/urls.py
@@ -7,4 +7,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', include('galeria.urls')),
     path('admin/', admin.site.urls),
+    path('', include('usuarios.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
